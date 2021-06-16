ab7fa499c9ea350bd943cde2b7fc831d2e6520ae
hazelisles <zhccj19@gmail.com>
2021-06-16 19:02:30 +0800

    feat: add User model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+const db = require('./models')
 
 app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
