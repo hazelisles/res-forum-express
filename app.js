9ad3809d3e2aec067ba9770f6c701d5fc94bfe99
hazelisles <zhccj19@gmail.com>
2021-06-16 15:58:14 +0800

    feat: set front-end entrance

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs({ extname: '.hbs' }))
-app.use('view engine', 'hbs')
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
