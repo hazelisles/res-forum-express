69904cb1b3c3966b7aff494b16f474cd6e3a9419
hazelisles <zhccj19@gmail.com>
2021-06-17 16:48:38 +0800

    feat: add connect-flash, express-session

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 const db = require('./models')
@@ -7,6 +9,17 @@ const db = require('./models')
 app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'SECRET',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
