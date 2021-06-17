5c430b78b62cb324433e5d67fcf44df10b93d719
hazelisles <zhccj19@gmail.com>
2021-06-17 23:59:57 +0800

    feat: add restaurant create feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,6 +9,19 @@ const adminController = {
   },
   createRestaurant: (req, res) => {
     return res.render('admin/create')
+  },
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '請輸入餐廳名稱')
+      return res.redirect('back')
+    }
+    const { name, tel, address, opening_hours, description } = req.body
+    return Restaurant.create({
+      name, tel, address, opening_hours, description
+    }).then((restaurant) => {
+      req.flash('success_messages', '成功新增餐廳！')
+      res.redirect('/admin/restaurants')
+    })
   }
 }
 module.exports = adminController
