80b143f8aaa59b9596f1c0c947ef8409ec2ab02c
hazelisles <zhccj19@gmail.com>
2021-06-17 22:52:05 +0800

    feat: add authentication for general users and admin

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,34 @@ const userController = require('../controllers/userController')
 const passport = require('passport')
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
+  app.get('/', authenticated, (req, res) => {
     res.redirect('/restaurants')
   })
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/admin', (req, res) => {
+  app.get('/admin', authenticatedAdmin, (req, res) => {
     res.redirect('/admin/restaurants')
   })
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
   app.get('/logout', userController.logout)
 }
