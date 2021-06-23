3828b2431abb9339355e4543c9bf28e120db8576
hazelisles <zhccj19@gmail.com>
2021-06-23 20:56:21 +0800

    feat: add category update feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,7 +4,15 @@ const categoryController = {
   getCategories: (req, res) => {
     return Category.findAll({
       raw: true, nest: true
-    }).then(categories => res.render('admin/categories', { categories }))
+    }).then(categories => {
+      if (req.params.id) {
+        Category.findByPk(req.params.id).then((category) => {
+          return res.render('admin/categories', { categories, category: category.toJSON() })
+        })
+      } else {
+        return res.render('admin/categories', { categories })
+      }
+    })
   },
   postCategory: (req, res) => {
     if (!req.body.name) {
@@ -12,6 +20,17 @@ const categoryController = {
       return res.redirect('back')
     } else {
       return Category.create({ name: req.body.name }).then((category) => res.redirect('/admin/categories'))
+    }
+  },
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '分類名稱不能空白！')
+      return res.redirect('back')
+    } else {
+      req.flash('success_messages', '成功修改分類！')
+      return Category.findByPk(req.params.id).then((category) => {
+        category.update(req.body).then(() => res.redirect('/admin/categories'))
+      })
     }
   }
 }
