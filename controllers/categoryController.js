1b92fac846a9f2edbf25d7ca8ddfdcdaa68c955e
hazelisles <zhccj19@gmail.com>
2021-06-23 19:39:07 +0800

    feat: add create category feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,6 +5,14 @@ const categoryController = {
     return Category.findAll({
       raw: true, nest: true
     }).then(categories => res.render('admin/categories', { categories }))
+  },
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '請輸入分類！')
+      return res.redirect('back')
+    } else {
+      return Category.create({ name: req.body.name }).then((category) => res.redirect('/admin/categories'))
+    }
   }
 }
 module.exports = categoryController
