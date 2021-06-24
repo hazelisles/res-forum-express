5be2730099a4c0d149b3057a4cef116a3c54fc82
hazelisles <zhccj19@gmail.com>
2021-06-24 15:01:12 +0800

    feat: add restaurant show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -12,6 +12,11 @@ const restController = {
       }))
       return res.render('restaurants', { restaurants: data })
     })
+  },
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
+      return res.render('restaurant', { restaurant: restaurant.toJSON() })
+    })
   }
 }
 module.exports = restController
