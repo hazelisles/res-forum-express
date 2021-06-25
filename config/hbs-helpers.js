ed9b3e9c069ae62eb2416f61122535169f467bc2
hazelisles <zhccj19@gmail.com>
2021-06-25 18:28:10 +0800

    feat: convert time format by moment

--- config/hbs-helpers.js
+++ config/hbs-helpers.js
@@ -1,8 +1,12 @@
+const moment = require('moment')
 module.exports = {
   eq: function (a, b, options) {
     if (a === b) {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+  howlong: function (a) {
+    return moment(a).fromNow()
   }
 }
