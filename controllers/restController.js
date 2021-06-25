9bd1822ebd8e2f23b5b8ba903314e79933cba8c5
hazelisles <zhccj19@gmail.com>
2021-06-25 17:07:07 +0800

    feat: show comments on related restaurant

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 
 const restController = {
@@ -32,7 +34,12 @@ const restController = {
     })
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
       return res.render('restaurant', { restaurant: restaurant.toJSON() })
     })
   }
