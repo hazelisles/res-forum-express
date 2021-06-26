b0e7d6761d7fd1a213061c899bf9773177dae592
hazelisles <zhccj19@gmail.com>
2021-06-26 20:22:10 +0800

    modify comments seeder

--- seeders/20210626065534-comments-seed-file.js
+++ seeders/20210626065534-comments-seed-file.js
@@ -4,8 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments', Array.from({ length: 20 }).map((data) => ({
       text: faker.lorem.sentence(),
-      UserId: Math.floor(Math.random() * 3 + 1) * 5,
-      RestaurantId: Math.floor(Math.random() * 20 + 1) * 5,
+      UserId: Math.floor(Math.random() * 3) * 10 + 5,
+      RestaurantId: Math.floor(Math.random() * 20) * 10 + 5,
       createdAt: new Date(),
       updatedAt: new Date(),
     })))
