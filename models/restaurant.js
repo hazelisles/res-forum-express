931065c506573252a1b69e53acf2d177a83ba360
hazelisles <zhccj19@gmail.com>
2021-06-27 18:15:26 +0800

    feat: add favorite model and set association between restaurant and user

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,14 +4,15 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       Restaurant.belongsTo(models.Category)
       Restaurant.hasMany(models.Comment)
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite,
+        foreignKey: 'RestaurantId',
+        as: 'FavoritedUsers'
+      })
     }
   };
   Restaurant.init({
